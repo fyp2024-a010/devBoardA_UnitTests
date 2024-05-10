bd36fb6a24cbf7cfadf093933677d9204601b799
Benjamin Teh <125846404+bentjh01@users.noreply.github.com>
2024-05-10 09:29:58 +0800

    Delete stm32_ws/protocolTest/Debug directory

--- stm32_ws/protocolTest/Debug/protocolTest/components/support/mf_crc.d
+++
@@ -1,4 +0,0 @@
-protocolTest/components/support/mf_crc.o: \
- ../protocolTest/components/support/mf_crc.c \
- ../protocolTest/components/support/mf_crc.h
-../protocolTest/components/support/mf_crc.h:
