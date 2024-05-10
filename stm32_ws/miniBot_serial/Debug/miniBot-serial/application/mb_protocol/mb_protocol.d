8e3ebb6109b4978a3cd997463915b0b39ffe2dd4
Benjamin Teh <125846404+bentjh01@users.noreply.github.com>
2024-05-10 09:29:34 +0800

    Delete stm32_ws/miniBot_serial/Debug directory

--- stm32_ws/miniBot_serial/Debug/miniBot-serial/application/mb_protocol/mb_protocol.d
+++
@@ -1,4 +0,0 @@
-miniBot-serial/application/mb_protocol/mb_protocol.o: \
- ../miniBot-serial/application/mb_protocol/mb_protocol.c \
- ../miniBot-serial/application/mb_protocol/mb_protocol.h
-../miniBot-serial/application/mb_protocol/mb_protocol.h:
